73e8fadc9897a7eb9e7012f37bb10657bcca54f1
Jimmy <jajalaba@yahoo.com.tw>
2021-10-22 13:48:08 +0800

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,8 +8,10 @@ const Error = () => {
       <SEO title="Error" />
       <main className="error-page">
         <section>
-          <h1>404</h1>
-          <h3>找不到你正在尋找的網頁</h3>
+          <h1>好車搶手</h1>
+          <h3>
+            你正在找的車子，可能已經賣掉，或者網路問題，所以找不到你正在尋找的網頁，請按左上角Logo回首頁，看看其他車子吧
+          </h3>
         </section>
       </main>
     </Layout>
